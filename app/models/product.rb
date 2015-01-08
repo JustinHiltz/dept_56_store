e76efde9b4007361a4959ee7b3f26e0634a37a7a
Justin Hiltz <justin4770@gmail.com>
2015-01-07 21:57:46 -0500

    Removed uniqueness validation from item_number

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
 	validates :item_number, :name, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-	validates :item_number, uniqueness: true
 end
