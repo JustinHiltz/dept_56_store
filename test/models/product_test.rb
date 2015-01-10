632170a54ee490432f686a8dffac15ecde710c9b
Justin Hiltz <justin4770@gmail.com>
2015-01-09 21:06:36 -0500

    Added intial test to product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:item_number].any?
+    assert product.errors[:name].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(item_number: "My Book Title",
+                          name:        "yyy",
+                          collection:  1)
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], 
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
 end
