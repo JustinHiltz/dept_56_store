fc71996dfec8a3f1ea0223423aa2a84a55689c6e
Justin Hiltz <justin4770@gmail.com>
2015-01-11 21:06:38 -0500

    Added orders funtionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,12 +55,14 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -75,6 +77,6 @@ class CartsController < ApplicationController
 
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}" 
-      redirect_to store_url, notice: 'Invalid cart'
+      redirect_to root_url, notice: 'Invalid cart'
     end
 end
